db96122bece86477dbff63f544d3555046d70fec
Kyle Kung <kylekung@hotmail.com>
2016-11-27 17:39:19 -0500

    set up cookie sessions

--- controllers.js
+++ controllers.js
@@ -1,22 +1,18 @@
 
 module.exports.register = ({body: {username, password}}, res) => {
-    console.log(username)
-    console.log(password)
     res.send("Register")
 }
 
-module.exports.login = ({body: {username, password}}, res) => {
-    console.log(username)
-    console.log(password)
+module.exports.login = (req, res) => {)
+    req.session.username = req.body.username
     res.send("Login")
 }
 
 module.exports.createNewApplication = (req, res) => {
-    console.log(req.body)
+    console.log(req.session)
     res.send("New Application")
 }
 
 module.exports.makeRecommendation = (req, res) => {
-    console.log(req.body)
     res.send("Make Recommendation")
 }
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieSession = require('cookie-session')
 var app = express()
-const controller = require("./controllers.js")
+const controller = require('./controllers.js')
 
 app.use(bodyParser.json())
 
+app.use(cookieSession({
+    name: 'session',
+    keys: ['coolestkey', 'secondcoolestkey'],
+    maxAge: 7 * 24 * 60 * 60 * 1000 // 1 week
+}))
+
 app.get('/', function(req,res){
-    res.send("Is working!")
+    res.send('Is working!')
 })
 
 app.post('/users', controller.register)
